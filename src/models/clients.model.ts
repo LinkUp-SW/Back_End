cf9598a4d13ea8ba6c2d602bad5dfd818ab007c8
Tarek-Waleed1 <tito020223@gmail.com>
2025-03-09 17:29:14 +0200

    feat(db): add enum functions for client sex and status, update schema logic

--- src/models/clients.model.ts
+++ src/models/clients.model.ts
@@ -8,69 +8,101 @@ import { licenseAndCertificationsInterface } from "./license_and_certifications.
 import { conversationsInterface } from "./conversations.model";
 import { activityInterface } from "./activity.model";
 import { jobsInterface } from "./jobs.model";
+import { reactsInterface } from "./reactions.model";
+import { usersInterface } from "./users.model";
 
 
+export enum sexEnum{
+    male="Male",
+    female="Female"
+}
+export enum statusEnum {
+    finding_new_job = "Finding a new job",
+    hiring = "Hiring",
+    providing_services = "Providing services",
+    finding_volunteer_opportunities = "Finding volunteer opportunities"
+  }
 export interface clientsInterface extends mongoose.Document{
-    bio: bioInterface;
-    education: educationsInterface[];
-    work_experience: experiencesInterface[];
-    skills: skillsInterface[];
-    liscence_certificates: licenseAndCertificationsInterface[];
-    industry: string;
-    profile_photo: string;
-    cover_photo: string;
-    resume: string;
-    connections: clientsInterface[];
-    followers: clientsInterface[];
-    following: clientsInterface[];
-    privacy_settings: privacySettingsInterface;
-    activity:activityInterface[]
-    status: Enumerator; 
-    blocked: clientsInterface[];
-    conversations: conversationsInterface[];
-    notification: Notification[];
-    applied_jobs: jobsInterface[];
-    saved_jobs: jobsInterface[];
-    sex: Enumerator;
-    subscription:{
-            subscribed: Boolean,
-            subscription_started_at:Date
-    };
-    is_student: boolean;
-    is_verified: boolean;
-    is_16_or_above: boolean;
+  client_id: usersInterface;
+  bio: bioInterface;
+  education: educationsInterface[];
+  work_experience: experiencesInterface[];
+  skills: skillsInterface[];
+  liscence_certificates: licenseAndCertificationsInterface[];
+  industry: string;
+  profile_photo: string;
+  cover_photo: string;
+  resume: string;
+  connections: clientsInterface[];
+  followers: clientsInterface[];
+  following: clientsInterface[];
+  privacy_settings: privacySettingsInterface;
+  activity:activityInterface[]
+  status: statusEnum; 
+  blocked: clientsInterface[];
+  conversations: conversationsInterface[];
+  notification: {
+      seen : boolean,
+      user_id :string,
+      sender_user_id:string,
+      conversation_id:string,
+      post_id:string,
+      comment_id:string,
+      react:reactsInterface
+  }[];
+  applied_jobs: jobsInterface[];
+  saved_jobs: jobsInterface[];
+  sex: sexEnum;
+  subscription:{
+          subscribed: Boolean,
+          subscription_started_at:Date
+  };
+  is_student: boolean;
+  is_verified: boolean;
+  is_16_or_above: boolean;
 }
 
 const clientsSchema = new mongoose.Schema<clientsInterface>({
-    bio: { type: Schema.Types.ObjectId, ref: "bio" },
-    education: [{ type: Schema.Types.ObjectId, ref: "educations" }],
-    work_experience: [{ type: Schema.Types.ObjectId, ref: "experiences" }],
-    skills: [{ type: Schema.Types.ObjectId, ref: "skills" }],
-    liscence_certificates: [{ type: Schema.Types.ObjectId, ref: "licenseAndCertifications" }],
-    industry: { type: String},
-    profile_photo: { type: String },
-    cover_photo: { type: String },
-    resume: { type: String },
-    connections: [{ type: Schema.Types.ObjectId, ref: "clients" }],
-    followers: [{ type: Schema.Types.ObjectId, ref: "clients" }],
-    following: [{ type: Schema.Types.ObjectId, ref: "clients" }],
-    privacy_settings: { type: Schema.Types.ObjectId, ref: "privacy_settings" },
-    activity: [{ type: Schema.Types.ObjectId, ref: "activity" }],
-    status: { type: Enumerator },
-    blocked: [{ type: Schema.Types.ObjectId, ref: "clients" }],
-    conversations: [{ type: Schema.Types.ObjectId, ref: "skills" }],
-    notification: [{type: Notification}],
-    applied_jobs:[{ type: Schema.Types.ObjectId, ref: "jobs" }],
-    saved_jobs:[{ type: Schema.Types.ObjectId, ref: "jobs" }],
-    sex: { type: Enumerator, required: true },
-    subscription:{
-            subscribed: Boolean,
-            subscription_started_at:Date
+  client_id:{ type: Schema.Types.ObjectId, ref: "users", required: true }, 
+  bio: { type: Schema.Types.ObjectId, ref: "bio" },
+  education: [{ type: Schema.Types.ObjectId, ref: "educations" }],
+  work_experience: [{ type: Schema.Types.ObjectId, ref: "experiences" }],
+  skills: [{ type: Schema.Types.ObjectId, ref: "skills" }],
+  liscence_certificates: [{ type: Schema.Types.ObjectId, ref: "licenseAndCertifications" }],
+  industry: { type: String},
+  profile_photo: { type: String },
+  cover_photo: { type: String },
+  resume: { type: String },
+  connections: [{ type: Schema.Types.ObjectId, ref: "clients" }],
+  followers: [{ type: Schema.Types.ObjectId, ref: "clients" }],
+  following: [{ type: Schema.Types.ObjectId, ref: "clients" }],
+  privacy_settings: { type: Schema.Types.ObjectId, ref: "privacy_settings" },
+  activity: [{ type: Schema.Types.ObjectId, ref: "activity" }],
+  status: { type: String, enum: Object.values(statusEnum), required: true },
+  blocked: [{ type: Schema.Types.ObjectId, ref: "clients" }],
+  conversations: [{ type: Schema.Types.ObjectId, ref: "skills" }],
+  notification: [
+      {
+        seen: { type: Boolean},
+        user_id: { type: String},
+        sender_user_id: { type: String},
+        conversation_id: { type: String},
+        post_id: { type: String},
+        comment_id: { type: String},
+        react: { type: Schema.Types.ObjectId, ref: "reacts" },
+      },
+    ],
+  applied_jobs:[{ type: Schema.Types.ObjectId, ref: "jobs" }],
+  saved_jobs:[{ type: Schema.Types.ObjectId, ref: "jobs" }],
+  sex: { type: String, enum: Object.values(sexEnum), required: true },
+  subscription: {
+      subscribed: { type: Boolean },
+      subscription_started_at: { type: Date },
     },
-    is_student: { type: Boolean, required: true },
-    is_verified: { type: Boolean, required: true },
-    is_16_or_above: { type: Boolean, required: true }
+  is_student: { type: Boolean, required: true },
+  is_verified: { type: Boolean, required: true },
+  is_16_or_above: { type: Boolean, required: true }
 });
 
 const clients = mongoose.model<clientsInterface>('clients',clientsSchema);
-export default clients 
+export default clients
