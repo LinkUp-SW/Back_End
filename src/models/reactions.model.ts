03ffb54b686cd85d738d0528e0cc46029250a72a
Tarek-Waleed1 <tito020223@gmail.com>
2025-03-09 17:31:11 +0200

    feat(db): add enum function for reactions and update schema logic

--- src/models/reactions.model.ts
+++ src/models/reactions.model.ts
@@ -1,13 +1,20 @@
 import mongoose, { Schema } from "mongoose";
+import { usersInterface } from "./users.model";
 
+export enum reactsEnum{
+    laugh = "laugh",
+    love = "heart",
+    like = "thumbs up",
+    cry = "cry"
+}
 
 export interface reactsInterface extends mongoose.Document{
-    user_id: string;
-    reaction: Enumerator;
+    user_id: usersInterface;
+    reaction: reactsEnum;
 }
 const reactsSchema = new Schema<reactsInterface>({
-    user_id: { type: String, required: true },
-    reaction: { type: String, required: true }
+    user_id: { type: Schema.Types.ObjectId, ref: "users", required:true },
+    reaction: { type: String, enum: Object.values(reactsEnum), required: true }
 });
 
 const reacts = mongoose.model<reactsInterface>('reacts', reactsSchema);
