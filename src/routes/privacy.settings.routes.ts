1c479d022d6be650ecaeaec2350bdb55e21ea12a
Mahmoudd29 <mahmoud.abdelhalim03@eng-st.cu.edu.eg>
2025-03-14 01:36:05 +0200

    feat(privacySettings): add routes for profile visibility endpoints

---
+++ src/routes/privacy.settings.routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as privacySettings from "../controllers/privacy.settings.controller.ts";
+
+const router = express.Router();
+
+
+router.get('/privacy-settings/profile-visibility/:user_id', privacySettings.getProfileVisibility);
+router.put('/privacy-settings/profile-visibility/:user_id', privacySettings.updateProfileVisibility);
+
+export default router;
