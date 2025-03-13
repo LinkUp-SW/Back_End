d08431fd52aa93499b5725723141cd0c92753a67
Tarek-Waleed1 <tito020223@gmail.com>
2025-03-13 17:12:41 +0200

    refactor(privacy-settings): replace boolean with enum for flag_account_status

--- src/models/privacy_settings.model.ts
+++ src/models/privacy_settings.model.ts
@@ -5,15 +5,22 @@ export enum invitationsEnum{
     email = "email",
 
 }
+
+export enum accountStatusEnum{
+    public = "Public",
+    private = "Private",
+    connections = "Connections only"
+}
+
 export interface privacySettingsInterface extends mongoose.Document{
-    flag_account_status: boolean;
+    flag_account_status: accountStatusEnum;
     flag_who_can_send_you_invitations: invitationsEnum;
     flag_messaging_requests: boolean;
     messaging_read_receipts: boolean;
 }
 
 const privacySettingsSchema = new Schema<privacySettingsInterface>({
-    flag_account_status:{ type: Boolean, required: true },
+    flag_account_status:{ type: String, enum: Object.values(accountStatusEnum), required: true },
     flag_who_can_send_you_invitations:{ type: String, enum: Object.values(invitationsEnum), required: true },
     flag_messaging_requests:{ type: Boolean, required: true },
     messaging_read_receipts:{ type: Boolean, required: true },
