099fa54be0a48a8a1644cc2451487b404256c1dd
Tarek-Waleed1 <tito020223@gmail.com>
2025-03-14 15:48:50 +0200

    feat(forget password): implement forget password functionality with email validation and error handling

--- src/services/forgetPassword.service.ts
+++ src/services/forgetPassword.service.ts
@@ -1,22 +1,29 @@
+import { error } from 'console';
 import nodemailer from 'nodemailer';
 
 
 
 const emailTransporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE, // e.g. 'Gmail'
+  service:"gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS
   },
 });
 
-
-export const sendResetPasswordEmail = async (email: string): Promise<void> => {
+export const sendResetPasswordEmail = async (email: string,resetLink: string): Promise<void> => {
+  try{
   const mailOptions = {
     from: process.env.EMAIL_FROM,
     to: email,
     subject: 'Reset password',
-    text: `reset password link:localhost:5174/reset-password`,
+    text: `reset password link:${resetLink}`,
   };
   await emailTransporter.sendMail(mailOptions);
+}catch (error){
+  throw error;
+}
 };
