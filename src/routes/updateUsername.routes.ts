c28609d062b5d268393e80fd79e9b671713da7dd
Tarek-Waleed1 <tito020223@gmail.com>
2025-03-16 03:11:30 +0200

    feat(user): add update username functionality

---
+++ src/routes/updateUsername.routes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { updateUsername } from '../controllers/updateUsername.Controller.ts';
+
+const router = express.Router();
+
+
+
+router.patch('/update-username', (req,res) =>{
+    updateUsername(req,res);
+});
+
+
+export default router;
