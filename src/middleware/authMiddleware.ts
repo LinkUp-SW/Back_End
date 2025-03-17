e972bd57d77f7babc16f05d89e4ab81d2e675f04
Mahmoudd29 <mahmoud.abdelhalim03@eng-st.cu.edu.eg>
2025-03-17 16:49:06 +0200

    refactor(authUpload): added validations for user_id and view_id

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import tokenUtils from "../utils/token.utils.ts";
-import { validateUserIdFromRequest, findUserByUserId } from "../utils/database.helper.ts";
+import { findUserByUserId, validateUserIdFromRequest, validateUserId } from "../utils/database.helper.ts";
 
 export const authorizeUpload = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
@@ -14,21 +14,27 @@ export const authorizeUpload = async (req: Request, res: Response, next: NextFun
       return;
     }
 
-    const viewerId = decodedToken.userId;
+    
 
     // Validate the user_id parameter from the request
     const userId = await validateUserIdFromRequest(req, res);
     if (!userId) return;
 
+    const viewerId = validateUserId(decodedToken.userId, res);
+    if (!viewerId) return;
+
     // Retrieve the user document
     const user = await findUserByUserId(userId, res);
     if (!user) return;
 
-    // Ensure the viewer is the same as the user (only the user can upload their own profile picture)
-    if (viewerId !== userId) {
+    // Ensure the viewer is the same as the user (only the user can upload their own resume)
+    if (decodedToken.userId !== userId) {
       res.status(403).json({ message: "You are not authorized to upload for this user." });
       return;
     }
+
+    // Attach the user object to the request for use in controllers
+    req.user = user;
 
     // If all checks pass, proceed to the next middleware
     next();
