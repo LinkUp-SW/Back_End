dfa2f2e7277bda895c5c502d274b6b073105593d
Hamza Elghonemy <elghonemyhamza04@gmail.com>
2025-03-21 23:51:25 +0200

    fix: update user location check to ensure 'country_region' is not empty

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -78,7 +78,7 @@ const googleCallback = asyncHandler(async (req: Request, res: Response, next: Ne
 
   }
 
-  if (Object.keys(userCheck.bio.location).length !== 0 && Object.keys(userCheck.bio.location).includes('country_region')) {
+  if (userCheck.bio.location.country_region && userCheck.bio.location.country_region.length === 0) {
     res.cookie("linkup_user_id", userCheck.user_id, {
       maxAge: 3600000,
       httpOnly: false,
