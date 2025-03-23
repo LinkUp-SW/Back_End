1b7ecc7bffd35d37f7caaef9cf6744c6a8004d40
Hamza Elghonemy <elghonemyhamza04@gmail.com>
2025-03-23 22:24:17 +0200

    feat: enhance OTP handling with logging and improved error message for verification

--- src/controllers/otp.controller.ts
+++ src/controllers/otp.controller.ts
@@ -28,6 +28,7 @@ const generateOTP = asyncHandler(async (req: Request, res: Response, next: NextF
   const otpCode = generateOTPCode();
 
   await sendEmailOTP(email, otpCode);
+  console.log(`OTP code for ${email}: ${otpCode}`);
 
   if (req.session) {
       req.session.otp = otpCode;
@@ -53,7 +54,7 @@ const verifyOTP = asyncHandler(async (req: Request, res: Response, next: NextFun
   }
 
   if (!req.session.otp || !req.session.otpEmail || email !== req.session.otpEmail) {
-      throw new CustomError('Invalid OTP', 401, 'INVALID_OTP');
+      throw new CustomError('Invalid OTP or email', 401, 'INVALID_OTP_MAIL');
   }
 
   if (req.session.otpExpires && Date.now() > req.session.otpExpires) {
