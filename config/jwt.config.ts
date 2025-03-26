a67d82bc89121abb7b4a3bfe4ad9b47e4b66f4b4
Hamza Elghonemy <elghonemyhamza04@gmail.com>
2025-03-26 16:50:44 +0200

    fix: change HTTP_ONLY setting in JWT config to false

--- config/jwt.config.ts
+++ config/jwt.config.ts
@@ -3,5 +3,5 @@ export const JWT_CONFIG = {
     SECRET: process.env.JWT_SECRET || '',
     EXPIRES_IN: '1h',
     COOKIE_NAME: 'linkup_auth_token',
-    HTTP_ONLY: true,
+    HTTP_ONLY: false,
   };
