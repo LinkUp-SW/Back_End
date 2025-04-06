2319a9f70a290301423c12564cfe58c66d193b86
Hamza Elghonemy <elghonemyhamza04@gmail.com>
2025-04-06 21:28:03 +0200

    fix(auth): correct casing in import statements and update user ID reference in AuthService

--- src/routes/authentication/resetPassword.routes.ts
+++ src/routes/authentication/resetPassword.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { resetPassword } from '../../controllers/authentication/resetPassword.Controller.ts';
+import { resetPassword } from '../../controllers/authentication/resetPassword.controller.ts';
 
 const router = express.Router();
 
