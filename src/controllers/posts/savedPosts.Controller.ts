ae1c74942cb63f203348f4223894d44ca767c027
Tarek-Waleed1 <tito020223@gmail.com>
2025-04-06 23:53:16 +0200

    feat(posts): implement save, display, and delete functionality for user saved posts

---
+++ src/controllers/posts/savedPosts.Controller.ts
@@ -0,0 +1,122 @@
+import { Request, Response } from 'express';
+import { findUserByUserId } from '../../utils/database.helper.ts';
+import { getSavedPostsCursorBased, PostRepository } from '../../repositories/posts.repository.ts';
+import { postsInterface } from '../../models/posts.model.ts';
+import { getUserIdFromToken } from '../../utils/helperFunctions.utils.ts';
+
+/**
+ * save post under the user
+ * Sends the appropriate response if post was saved successfully.
+ * 
+ */
+
+
+const savePost = async (req: Request, res: Response): Promise<Response | void> =>{
+    try {
+
+        const {
+            postId
+        } =req.body;
+        if (!postId){
+            return res.status(400).json({message:'Required fields missing' })
+        }
+        let userId = await getUserIdFromToken(req,res);
+        if (!userId) return;
+        const user = await findUserByUserId(userId,res);
+        if (!user) return;
+        const postRepository = new PostRepository;
+        const post = await postRepository.findByPostId(postId);
+        if (!post){
+            return res.status(404).json({message:'Post does not exist ' })
+        }
+        // add saved post to the user
+        user.savedPosts.push(postId);
+
+        // Save the updated user
+        await user.save();
+        return res.status(200).json({message:'Post successfully saved for user' })
+    } catch (error) {
+        if (error instanceof Error && error.message === 'Invalid or expired token') {
+            return res.status(401).json({ message: error.message,success:false });
+        }
+        else{
+            res.status(500).json({ message: 'Server error', error });
+    
+        }
+    }
+};
+/**
+ * Display the saved posts under
+ * Sends the posts in batches and the index to start the next batch from
+ * 
+ */
+const displaySavedPosts = async (req: Request, res: Response): Promise<Response | void> =>{
+    try {
+
+        const {
+            limit,
+            cursor
+        } =req.body;
+        if (!limit ){
+            return res.status(400).json({message:'Required fields missing' })
+        }
+        let userId = await getUserIdFromToken(req,res);
+        if (!userId) return;
+        const user = await findUserByUserId(userId,res);
+        if (!user) return;
+        const savedPosts = user.savedPosts.map((post: postsInterface) => post._id);
+        const { posts: postsData, nextCursor } = await getSavedPostsCursorBased(savedPosts as string[], cursor, limit);
+
+        return res.status(200).json({message:'Posts returned successfully',posts:postsData,nextCursor:nextCursor })
+    } catch (error) {
+        if (error instanceof Error && error.message === 'Invalid or expired token') {
+            return res.status(401).json({ message: error.message,success:false });
+        }
+        else{
+            res.status(500).json({ message: 'Server error', error });
+    
+        }
+    }
+}; 
+
+
+/**
+ * delete saved post under the user
+ * Sends the appropriate response if saved post was deleted successfully.
+ * 
+ */
+
+const deleteSavedPost = async (req: Request, res: Response): Promise<Response | void> =>{
+    try {
+
+        const {
+            postId
+        } =req.body;
+        if (!postId ){
+            return res.status(400).json({message:'Required fields missing' })
+        }
+        let userId = await getUserIdFromToken(req,res);
+        if (!userId) return;
+        const user = await findUserByUserId(userId,res);
+        if (!user) return;
+        const postRepository = new PostRepository;
+        const post = await postRepository.findByPostId(postId);
+        if (!post){
+            return res.status(404).json({message:'Post does not exist ' })
+        }
+         // Remove the post from the savedPosts array using filter
+         user.savedPosts = user.savedPosts.filter(savedPostId => savedPostId.toString() !== postId);
+         await user.save();
+         return res.status(200).json({message:'Deleted saved post successfully for user' })
+} catch (error) {
+        if (error instanceof Error && error.message === 'Invalid or expired token') {
+            return res.status(401).json({ message: error.message,success:false });
+        }
+        else{
+            res.status(500).json({ message: 'Server error', error });
+    
+        }
+    }
+}; 
+
+export {savePost,displaySavedPosts,deleteSavedPost};
