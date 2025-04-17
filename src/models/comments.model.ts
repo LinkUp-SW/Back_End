5891c881de0eac2a77ee0f3ba58da44ded3a4724
Tarek-Waleed1 <tito020223@gmail.com>
2025-04-17 21:33:08 +0200

    fix(comments): Update comments model to correct user_id type and add parentId and media field adjustments

--- src/models/comments.model.ts
+++ src/models/comments.model.ts
@@ -5,22 +5,26 @@ import { postsInterface } from "./posts.model.ts";
 
 export interface commentsInterface extends mongoose.Document{
     post_id: postsInterface;
-    user_id: string;
+    parentId:commentsInterface;
+    user_id: usersInterface;
     content: string;
     date: Date;
-    media: string[];
+    media: string;
     reacts: string[];
     tagged_users: string[];
+    is_edited:boolean;
 } 
 
 const commentsSchema = new Schema<commentsInterface>({
     post_id: { type: Schema.Types.ObjectId, ref: "posts"},
-    user_id: { type: String },
+    parentId:{type: Schema.Types.ObjectId, ref: "comments"},
+    user_id: {type: Schema.Types.ObjectId, ref: "users"},
     content: { type: String},
     date: { type: Date, default: Date.now },
-    media: [{ type: String }],
+    media: { type: String },
     reacts: [{ type: String }],
     tagged_users: [{ type: String }],
+    is_edited:{type:Boolean, default:false}
 });
 const comments =  mongoose.model<commentsInterface>('comments', commentsSchema);
 
