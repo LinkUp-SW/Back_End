693a203ced04dbeb0ed777e281fe24dad61d32a5
Mahmoudd29 <mahmoud.abdelhalim03@eng-st.cu.edu.eg>
2025-04-19 15:20:56 +0200

    fix(routes): Correct import statement for search suggestions controller

--- src/routes/my_network/userSearch.routes.ts
+++ src/routes/my_network/userSearch.routes.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { searchUsersController } from '../../controllers/my_network/userSearch.controller.ts';
-
+import { searchSuggestionsController } from '../../controllers/my_network/searchSuggestions.controller.ts';
 const router = express.Router();
 
 router.get('/users', searchUsersController);
-
+router.get('/suggestions', searchSuggestionsController);
 export default router;
