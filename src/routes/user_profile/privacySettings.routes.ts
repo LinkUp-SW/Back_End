2d185d81823297b24e3027506d7e31ec9b5f6fb0
Mahmoudd29 <mahmoud.abdelhalim03@eng-st.cu.edu.eg>
2025-04-19 21:11:40 +0200

    feat(priacySettings.routes): Add GET and PUT endpoints for invitations and follow requests

--- src/routes/user_profile/privacySettings.routes.ts
+++ src/routes/user_profile/privacySettings.routes.ts
@@ -7,7 +7,11 @@ const router = express.Router();
 router.get('/privacy-settings/profile-visibility', privacySettings.getProfileVisibility);
 router.put('/privacy-settings/profile-visibility', privacySettings.updateProfileVisibility);
 
+router.get('/privacy-settings/invitations-requests', privacySettings.getInvitationSettings);
+router.put('/privacy-settings/invitations-requests', privacySettings.updateInvitationSettings);
 
+router.get('/privacy-settings/follow-requests', privacySettings.getFollowSettings);
+router.put('/privacy-settings/follow-requests', privacySettings.updateFollowSettings);
 
 
 export default router;
