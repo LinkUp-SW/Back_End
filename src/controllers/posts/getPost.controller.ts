02531898a157bc57e7906f4418788f6cf2b43b14
Tarek-Waleed1 <tito020223@gmail.com>
2025-04-26 23:44:47 +0300

    feat(posts): enhance getPost controller to include user reaction and pass user ID to getComments

--- src/controllers/posts/getPost.controller.ts
+++ src/controllers/posts/getPost.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { findUserByUserId } from '../../utils/database.helper.ts';
 import { PostRepository } from '../../repositories/posts.repository.ts';
-import { getTopReactions } from '../../repositories/reacts.repository.ts';
+import { getTopReactions, ReactionRepository } from '../../repositories/reacts.repository.ts';
 import { getUserIdFromToken } from '../../utils/helperFunctions.utils.ts';
 import users from '../../models/users.model.ts';
 import { getComments } from '../../repositories/comment.repository.ts';
@@ -39,7 +39,7 @@ const getPost = async (req: Request, res: Response): Promise<Response | void> =>
             profilePicture: postAuthor.profile_photo,
             connectionDegree:"3rd+"
         };
-        const result = await getComments(cursor, limit, postId,replyLimit);
+        const result = await getComments(cursor, limit, postId,replyLimit,user._id as string);
         const plainPost = post.toObject ? post.toObject() : post;
         if (plainPost.tagged_users && plainPost.tagged_users.length > 0) {
             const userIds = await convert_idIntoUser_id(plainPost.tagged_users);
@@ -51,7 +51,9 @@ const getPost = async (req: Request, res: Response): Promise<Response | void> =>
             savedPostId.toString() === postId
           );        
         const reactions = await getTopReactions(postId, targetTypeEnum.post);
-          return res.status(200).json({message:'Post returned successfully',post:{...plainPost, author:authorInfo,isSaved,reactions: reactions.topReacts,reactionsCount: reactions.totalCount},comments:result })
+        const reactionRepository = new ReactionRepository;
+        const userReaction = await reactionRepository.getUserReaction(user._id as string,postId);
+        return res.status(200).json({message:'Post returned successfully',post:{...plainPost, author:authorInfo,isSaved, userReaction:userReaction?.reaction?? null ,reactions: reactions.topReacts,reactionsCount: reactions.totalCount,},comments:result })
     } catch (error) {
         if (error instanceof Error && error.message === 'Invalid or expired token') {
             return res.status(401).json({ message: error.message });
