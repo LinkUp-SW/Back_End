a43d91ae72e7d281dec8c9dec4e1ffa94fe57785
Tarek-Waleed1 <tito020223@gmail.com>
2025-04-30 19:14:21 +0300

    feat(posts): enhance post deletion logic to handle reposts and user activity updates

--- src/controllers/posts/deletePosts.controller.ts
+++ src/controllers/posts/deletePosts.controller.ts
@@ -4,6 +4,7 @@ import { findUserByUserId } from '../../utils/database.helper.ts';
 import { PostRepository } from '../../repositories/posts.repository.ts';
 import { deleteAllComments } from '../../repositories/comment.repository.ts';
 import { deleteAllPostReactions, deleteCommentReactions } from '../../repositories/reacts.repository.ts';
+import { postTypeEnum } from '../../models/posts.model.ts';
 
 
 /**
@@ -31,8 +32,24 @@ const deletePost = async (req: Request, res: Response): Promise<Response | void>
         }
         await deleteAllPostReactions(postId);
         await deleteAllComments(postId);
+        if(post.reposts){
+            const repostIds = post.reposts.map(repost => repost.toString());
+            await postRepository.deleteAllRepostsOfPost(repostIds);
+
+        }
         // remove post from the user
-        user.activity.posts = user.activity.posts.filter((userPost) => userPost.toString() !== postId);
+        if (post.post_type === postTypeEnum.standard){
+            user.activity.posts = user.activity.posts.filter((userPost) => userPost.toString() !== postId);
+
+        } else{
+            user.activity.reposted_posts = user.activity.reposted_posts.filter((userPost) => userPost.toString() !== postId);
+            const originalPost = await postRepository.findByPostId(post.media.link[0]);
+            if (originalPost){
+                originalPost.reposts = originalPost.reposts!.filter((userPost) => userPost.toString() !== postId);
+                await originalPost.save();
+            }
+
+        }
 
         // Save the updated user
         await user.save();
