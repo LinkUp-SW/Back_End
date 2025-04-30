b940a76b4b436e838db46315ac25e61a6f4b01af
Tarek-Waleed1 <tito020223@gmail.com>
2025-04-30 19:04:43 +0300

    feat(posts, users): add repost functionality and update user activity schema

--- src/models/posts.model.ts
+++ src/models/posts.model.ts
@@ -18,13 +18,20 @@ export enum mediaTypeEnum{
     link = "link",
     none = "none"
 }
-export interface postsInterface extends mongoose.Document{
+
+export enum postTypeEnum {
+    standard = "Standard",
+    repost_thought = "Repost thought",
+    repost_instant = "Repost instant"
+}
+
+export interface postsInterface extends mongoose.Document {
     user_id: string;
     content: string;
-    date: number; // Changed to number type for Unix timestamp
+    date: number; 
     media: {
-        link:string[],
-        media_type:string
+        link: string[],
+        media_type: string
     };
     comments_disabled: commentsEnum;
     public_post: Boolean;
@@ -32,25 +39,37 @@ export interface postsInterface extends mongoose.Document{
     tagged_users: string[];
     comments: commentsInterface[];
     isEdited:boolean;
-} 
+    
+    // Fields to support reposts
+    post_type: postTypeEnum;
+    reposts?: postsInterface[]
+}
 
 const postsSchema = new Schema<postsInterface>({
     user_id: { type: String },
     content: { type: String },
     date: { 
         type: Number, 
-        default: () => Math.floor(Date.now() / 1000) // Unix timestamp in seconds
+        default: () => Math.floor(Date.now() / 1000)
     },
     media: { 
-        link:[{type: String}],
-        media_type: {type: String}
-     },
+        link: [{ type: String }],
+        media_type: { type: String }
+    },
     comments_disabled: { type: String, enum: Object.values(commentsEnum) },
     public_post: { type: Boolean, default: true },
     reacts: [{ type: Schema.Types.ObjectId, ref: "reacts" }],
     tagged_users: [{ type: String }],
     comments: [{ type: Schema.Types.ObjectId, ref: "comments" }],
-    isEdited:{type:Boolean,default:false}
+    isEdited: { type: Boolean, default: false },
+    
+    // New fields for combined schema
+    post_type: { 
+        type: String, 
+        enum: Object.values(postTypeEnum), 
+        default: postTypeEnum.standard 
+    },
+    reposts: [{ type: Schema.Types.ObjectId, ref: 'posts' }]
 });
 
 const posts = mongoose.model<postsInterface>('posts', postsSchema);
