d8b84ccbc90998e903e66f3b906b2162fe95e9d7
Tarek-Waleed1 <tito020223@gmail.com>
2025-04-30 19:08:02 +0300

    feat(posts): enhance post creation with original post verification and repost handling

--- src/controllers/posts/createPosts.controller.ts
+++ src/controllers/posts/createPosts.controller.ts
@@ -3,7 +3,7 @@ import { findUserByUserId } from '../../utils/database.helper.ts';
 import { processPostMediaArray } from '../../services/cloudinary.service.ts';
 import { PostRepository } from '../../repositories/posts.repository.ts';
 import { getUserIdFromToken } from '../../utils/helperFunctions.utils.ts';
-import { mediaTypeEnum } from '../../models/posts.model.ts';
+import { mediaTypeEnum, postsInterface, postTypeEnum } from '../../models/posts.model.ts';
 import { convertUser_idInto_id } from '../../repositories/user.repository.ts';
 
 
@@ -23,15 +23,19 @@ const createPost = async (req: Request, res: Response): Promise<Response | void>
             media,
             commentsDisabled,
             publicPost,
-            taggedUsers
+            taggedUsers,
+            postType,
         } =req.body;
         let userId = await getUserIdFromToken(req,res);
         if (!userId) return;
         const user = await findUserByUserId(userId,res);
         if (!user) return;
-        if ( (!content && !media) || !commentsDisabled || publicPost == null || publicPost === undefined){
-            return res.status(400).json({message:'Required fields missing' })
+        let originalPost;
+        if ((!content && !media) || ((!commentsDisabled || publicPost == null) && postType !== postTypeEnum.repost_instant)) {
+            return res.status(400).json({ message: 'Required fields missing' });            
+        
         }
+        
         let processedMedia: string[] | null = null;
         switch (mediaType) {
             case mediaTypeEnum.none:
@@ -41,7 +45,20 @@ const createPost = async (req: Request, res: Response): Promise<Response | void>
                 processedMedia=media;
                 break;
             case mediaTypeEnum.post:
+                // Verify the original post exists
+                originalPost = await new PostRepository().findByPostId(media);
+                if (!originalPost) {
+                return res.status(404).json({ message: 'Original post not found' });
+                }
                 processedMedia=media;
+                if (!postType) return res.status(400).json({ message: 'postType is required' });
+                if (
+                  postType === postTypeEnum.repost_instant &&
+                  (content || commentsDisabled || publicPost)
+                )
+                  return res
+                    .status(400)
+                    .json({ message: "cannot add content to instant reposts" });
                 break;
             default:
                 if (media) {
@@ -54,6 +71,7 @@ const createPost = async (req: Request, res: Response): Promise<Response | void>
         if (taggedUsers)
             { converted_id = await convertUser_idInto_id(taggedUsers);}
         const postRepository = new PostRepository;
+        console.log(postType)
         const newPost = await postRepository.create(
             user._id!.toString(),
             content,
@@ -61,12 +79,21 @@ const createPost = async (req: Request, res: Response): Promise<Response | void>
             mediaType,
             commentsDisabled,
             publicPost,
+            postType,
             converted_id
         );
         await newPost.save();
-        user.activity.posts.push(newPost);
+        
+        if(!(postType === postTypeEnum.standard) && originalPost){
+            originalPost.reposts = originalPost.reposts || [];
+            originalPost.reposts.push(newPost._id as postsInterface);
+            await originalPost.save();
+            user.activity.reposted_posts.push(newPost);
+        }else{
+            user.activity.posts.push(newPost);
+        }
         await user.save();
-        return res.status(200).json({message:'Post successfully created', postId:newPost._id })
+        return res.status(200).json({message:'Post successfully created', postId:newPost._id,postType })
     } catch (error) {
         if (error instanceof Error && error.message === 'Invalid or expired token') {
             return res.status(401).json({ message: error.message,success:false });
