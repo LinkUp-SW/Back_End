af0f6cf5ba71167ab5e004378bce47b0c890772c
Hamza Elghonemy <elghonemyhamza04@gmail.com>
2025-05-04 00:26:42 +0300

    fix(index): change database connection to use test database

--- index.ts
+++ index.ts
@@ -84,7 +84,7 @@ const generateStartupToken = () => {
 };
 
 
-connectToDatabase()
+connectToTestDatabase()
   .then(() => {
     server.listen(PORT, () => {
       console.log('Server is running on port:', PORT);
