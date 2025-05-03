89abf97c9257801d5d111db168e02852063a41d6
Tarek-Waleed1 <tito020223@gmail.com>
2025-05-03 18:04:58 +0300

    fix(routes): rename dashboard route import for consistency and update token userID

--- index.ts
+++ index.ts
@@ -56,7 +56,7 @@ import subscriptionRoutes from './src/routes/subscription/subscription.routes.ts
 import peopleYouMayKnowRoutes from './src/routes/my_network/peopleYouMayKnow.routes.ts';
 import userSearchRoutes from './src/routes/my_network/userSearch.routes.ts';
 import reportRoutes from './src/routes/admin/report.routes.ts'
-import dashboard from './src/routes/admin/dashboard.routes.ts'
+import dashboardRoutes from './src/routes/admin/dashboard.routes.ts'
 
 dotenv.config();
 
@@ -73,7 +73,7 @@ app.use(express.json({limit:"50mb"}));
 
 // Generate a token with a 1-hour expiration and user_id "TiTo-aggin93"
 const generateStartupToken = () => {
-  const token = tokenUtils.createToken({ time: '1000h', userID: 'hamza-ayman-1745264575839' });
+  const token = tokenUtils.createToken({ time: '1000h', userID: 'Amr-Doma81' });
   console.log('Generated Token:', token);
 };
 
@@ -189,7 +189,7 @@ app.use('/api/v1/search',
 app.use('/api/v1/admin',
    createAdminRoutes,
    reportRoutes,
-   dashboard
+   dashboardRoutes
   );
 
 app.get('/', (req: Request, res: Response) => {
